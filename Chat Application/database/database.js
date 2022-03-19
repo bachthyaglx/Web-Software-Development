cce6caa14fb0b84ba8411427449770796dc9b956
Thy Khuu <62774638+bachthyaglx@users.noreply.github.com>
2022-03-19 07:40:50 +0200

    Add files via upload

--- Chat Application/database/database.js
+++ Chat Application/database/database.js
@@ -1,6 +1,17 @@
 import { Pool } from "https://deno.land/x/postgres@v0.14.3/mod.ts";
 const CONCURRENT_CONNECTIONS = 2;
-const connectionPool = new Pool({/*Database Credentials*/}, CONCURRENT_CONNECTIONS);
+let connectionPool;
+if (Deno.env.get("DATABASE_URL")) {
+  connectionPool = new Pool(Deno.env.get("DATABASE_URL"), CONCURRENT_CONNECTIONS);
+} else {
+  connectionPool = new Pool({
+    hostname: "ec2-54-220-195-236.eu-west-1.compute.amazonaws.com",
+    database: "d1m4a6gmp5vgd7",
+    user: "qghtkvpyjnhnpw",
+    password: "d098aacfb0b4d20656e193d28bc34353f3dcbe49ace70ecbd705a5b5369388ae",
+    port: "5432",
+  }, CONCURRENT_CONNECTIONS);
+}
 
 const executeQuery = async (query, ...args) => {
   const response = {};
